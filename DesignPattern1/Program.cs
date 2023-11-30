a54e731dd74232975e40ccf39765064ea85a380d
Penahli Ibrahim <penahliibrahim58@gmail.com>
2023-11-30 23:08:20 +0400

    Design Pattern 1

---
+++ DesignPattern1/Program.cs
@@ -0,0 +1,53 @@
+﻿public interface IButton
+{
+    void render();
+    void onClick();
+}
+
+public class Dialog
+{
+    public void render() { }
+    public virtual IButton createButton () { }
+}
+
+public class WindowsButton : IButton
+{
+    public void onClick()
+    {
+        // On Click
+    }
+
+    public void render()
+    {
+        // Render
+    }
+}
+
+public class HTMLButton : IButton
+{
+    public void onClick()
+    {
+        // On Click
+    }
+
+    public void render()
+    {
+        // Render
+    }
+}
+
+public class WindowsDialog : Dialog
+{
+    public override IButton createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+public class WebDialog : Dialog
+{
+    public override IButton createButton()
+    {
+        return new HTMLButton();
+    }
+}
